dc9f5ef5348d4e7f15ce51391750acad1e3e8cb2
kalifkhan <85588700+kalifkhan@users.noreply.github.com>
2023-09-05 09:23:08 +0000

    changes Updating

--- src/api/UserApi.js
+++ src/api/UserApi.js
@@ -7,33 +7,32 @@ const UserApi = () => {
     const [email , setEmail] = useState();
     const [ fullname , setFullname] = useState();
 
-    useEffect(() => {
-
-        const fetchInfo = async () => {
-            try {
-                const response = await axios.get("https://randomuser.me/api");
-                localStorage.setItem( 'userDataInfo'  , JSON.stringify(response.data.results[0]));
-            } catch (error) {
-                console.log("errror is " + error);
-            }
-        };
-        fetchInfo();
-
-    }, [refreshFlag]);
-
-    
-    useEffect( ()=>{
+    // fetch function and setItem into Localstorage 
+    const fetchInfo = async () => {
+        try {
+            const response = await axios.get('https://randomuser.me/api');
+            localStorage.setItem( 'userDataInfo'  , JSON.stringify(response.data.results));
+        } catch (error) {
+            console.log("errror is " + error);
+        }
+    };
+    // parse function and getItem from localstorage into state(key)
+    const ParseUserData=()=>{
         const parseJson = localStorage.getItem('userDataInfo');
         if(parseJson){
             const ParsedJSON = JSON.parse(parseJson);
-            console.log(parseJson);
+            //console.log(parseJson);
 
-            const { email , name} = ParsedJSON;
+            const { email , name} = ParsedJSON[0];
             setFullname( name.first + ' ' + name.last);
             setEmail(email);
-            //console.log(email)
         }
-    } , [refreshFlag]);
+    }
+    //useEffect to call a API and Display the userInfo whenever refreshed button changes. 
+    useEffect(() => {
+        fetchInfo();
+        ParseUserData();
+    }, [refreshFlag]);
 
     const handleRefresh=()=>{
         setFlag(!refreshFlag);
